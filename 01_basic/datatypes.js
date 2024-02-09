148c2abbd774bd21ce1d811a66141d95ea64bf53
prime-jd <120632610+prime-jd@users.noreply.github.com>
2024-02-09 20:40:11 +0000

    datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,14 @@
+let age = 45
+let name = "ravi"
+let blood = null
+let address;
+let degree = true
+//number
+//bigInt
+//boolean 
+//null
+//undefined
+//string
+//object
+
+console.table([typeof age,typeof name,typeof blood,typeof address,typeof degree])

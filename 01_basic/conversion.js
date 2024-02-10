b607367b3ac72d0c444c632bf7819c477d7c6cb9
prime-jd <120632610+prime-jd@users.noreply.github.com>
2024-02-10 11:16:35 +0000

    conversion datatypes

---
+++ 01_basic/conversion.js
@@ -0,0 +1,17 @@
+// let name = "ravi"
+// let nme = Number(name)
+// console.log(typeof nme)
+// console.log(nme)
+//conversion of string not containing all numbers gives NaN value
+
+// let name = ""
+// let nme = Number(name)
+// console.log(typeof nme)
+// console.log(nme)
+
+let x =11
+let y = Boolean(x)
+console.log(typeof y)
+console.log(y)
+// numbers other than 0 converted to true in boolean
+//similarly a empty string gives false value and non empty gives true

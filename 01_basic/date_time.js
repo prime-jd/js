482b5913af18325f838d64789661a69aac030bf2
prime-jd <120632610+prime-jd@users.noreply.github.com>
2024-02-11 16:18:50 +0000

    date and time

---
+++ 01_basic/date_time.js
@@ -0,0 +1,10 @@
+const d = new Date()
+console.log(d);
+console.log(d.toLocaleString());
+console.log(d.toLocaleDateString());
+console.log(typeof d)  // date is object data type
+
+const mydate = new Date(2002, 11, 1)
+console.log(mydate)
+
+console.log(mydate.getTime());

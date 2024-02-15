d7b8f309c5d71140547b09b231f3ae6eed6f100b
prime-jd <120632610+prime-jd@users.noreply.github.com>
2024-02-15 23:24:38 +0000

    switch statements

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,19 @@
+//switch statements
+const ch = 4; 
+switch (ch) {    //it can also take string values
+    case 1:
+        console.log("hey it's 1")
+        break;
+    case 2:
+        console.log("hey it's 2")
+        break;
+    case 3:
+        console.log("hey it's 3")
+        break;
+    case 4:
+        console.log("hey it's 4")
+        break;
+    default:
+        console.log("jai shree ram")
+        break;
+}

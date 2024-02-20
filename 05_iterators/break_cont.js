0567639514c387f0a27b5c6b673492ed75d20372
prime-jd <120632610+prime-jd@users.noreply.github.com>
2024-02-20 17:03:28 +0000

    break and continue

---
+++ 05_iterators/break_cont.js
@@ -0,0 +1,13 @@
+for (let i = 0; i < 10; i++) {
+    if(i==5)
+    continue;                  // ignores the iteration when  condition met
+console.log(i)       
+    
+}
+for (let i = 0; i < 10; i++) {
+    if(i==5)
+    break;                  // jumps out of the loop when condition is met
+console.log(i)       
+    
+}
+ 

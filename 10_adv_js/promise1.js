ee881093f0f70dc921ef0f925dbc2507e3ad440f
prime-jd <120632610+prime-jd@users.noreply.github.com>
2024-03-11 20:53:34 +0000

    promises and api requests

--- 10_adv_js/promise1.js
+++ 10_adv_js/promise1.js
@@ -1,24 +1,40 @@
 // promises => A Promise is an object representing the eventual completion or failure of an asynchronous operation
 // it takes two arguments 
 //create promise
-const promiseOne = new Promise(function(resolve, reject) {
-    // Perform any asynchronous operation
-    // For example, DB calls, cryptography, etc.
-    let req;
-    
-     fetch('https://api.github.com/users/prime-jd') 
-        //console.log(req)
-        resolve();
-});
 
-promiseOne.then(function(user) {
-    //console.log(user);
-     return user.json();
-}).then(function(data){
-    console.log(data)
-}).catch(function(error) {
-    console.error('Error occurred:', error);
-});
+// Function to fetch data from API
+// function fetchData(url) {
+//     return new Promise((resolve, reject) => {
+//         fetch(url)
+//             .then(response => {
+//                 if (!response.ok) {
+//                     throw new Error('Network response was not ok');
+//                 }
+//                 return response.json(); // Parse JSON response
+//             })
+//             .then(data => {
+//                 resolve(data); // Resolve with the parsed data
+//             })
+//             .catch(error => {
+//                 reject(error); // Reject with the error
+//             });
+//     });
+// }
+
+// // Usage example
+// const apiUrl = 'https://api.github.com/users/prime-jd';
+// fetchData(apiUrl)
+//     .then(data => {
+//         console.log(data); // Access data from the API
+//         // Process the data further as needed
+//     })
+//     .catch(error => {
+//         console.error('There was a problem fetching data:', error);
+//     });
+
+// we can request data from apis either using 
+//1. XHR(XMLHttpRequest)
+//2.FETCH(URL)
 
 // Function to fetch data from API
 function fetchData(url) {
@@ -49,5 +65,3 @@ fetchData(apiUrl)
     .catch(error => {
         console.error('There was a problem fetching data:', error);
     });
-
-
